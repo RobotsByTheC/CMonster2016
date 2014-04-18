c106e5679d949cdb64c3bef4ac1d1b1e759c1db2
Ben W <benwolsieffer@gmail.com>
2014-04-18 13:04:52 -0400

    Add license header

--- src/edu/wpi/first/wpilibj/BetterGyro.java
+++ src/edu/wpi/first/wpilibj/BetterGyro.java
@@ -1,3 +1,9 @@
+/* 
+ * Copyright (c) 2014 RobotsByTheC. All rights reserved.
+ *
+ * Open Source Software - may be modified and shared by FRC teams. The code must
+ * be accompanied by the BSD license file in the root directory of the project.
+ */
 package edu.wpi.first.wpilibj;
 
 /**
--- src/org/usfirst/frc2084/CMonster2014/commands/SweeperRetractCommand.java
+++ src/org/usfirst/frc2084/CMonster2014/commands/SweeperRetractCommand.java
@@ -1,3 +1,9 @@
+/* 
+ * Copyright (c) 2014 RobotsByTheC. All rights reserved.
+ *
+ * Open Source Software - may be modified and shared by FRC teams. The code must
+ * be accompanied by the BSD license file in the root directory of the project.
+ */
 package org.usfirst.frc2084.CMonster2014.commands;
 
 import org.usfirst.frc2084.CMonster2014.Robot;
